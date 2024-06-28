c1e56bee14bde50cd51aba6ce57c44ef106a3f4a
Lucas Braga Santos <lucasbrasantos@gmail.com>
2024-06-28 16:25:13 -0300

    Update Insect.java

--- src/module5/lesson10/Insect.java
+++ src/module5/lesson10/Insect.java
@@ -7,15 +7,21 @@ public class Insect {
     private int x;
     private int y;
 
-    private final double DIST_WEIGHT_LOSS_FACTOR = .0001;
+    private static final double DIST_WEIGHT_LOSS_FACTOR = .0001;
+    private static int population = 0;
+    private static String[] FACTS = {
+        "The two main groups of insects are winged and wingless",
+        "There are more than 1 million insects species",
+        "Spiders are not considered insects"
+    };
 
     // -- Constructor
     public Insect(double initWeight, int initX, int initY){
         weight = initWeight;
         x = initX;
         y = initY;
+        population++;
     }
-
     public void eat(double amount){
         System.out.println("Nhum Nhum Whomp Whomp");
         weight = weight + amount;
@@ -37,6 +43,12 @@ public class Insect {
         return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
     }
 
+    public static String produceRandomFact(){
+        int index = (int)(Math.random() * ((FACTS.length -1) + 1));
+        return FACTS[index];
+    }
+
+
     // -- Test method
     public static void main(String[] args) {        
         // Insect buzz1 = new Insect(); without constructor this is fine because takes the default values
@@ -46,6 +58,8 @@ public class Insect {
         Insect bug2 = new Insect(10, 20, -20);
 
         bug1.move(34, 12);
+
+        System.out.println(produceRandomFact());
 
 
     }
