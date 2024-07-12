2fe1c50163b38c71f984be7a56bf93b6b337b705
Lucas Braga Santos <lucasbrasantos@gmail.com>
2024-07-12 01:02:25 -0300

    Update SortingMethods.java
    merge sort

--- src/module6/lesson13/SortingMethods.java
+++ src/module6/lesson13/SortingMethods.java
@@ -2,6 +2,7 @@ package module6.lesson13;
 
 public class SortingMethods {
     
+        public static int aa= 0;
 
     public static void intSelectionSort(int[] list){
 
@@ -47,7 +48,7 @@ public class SortingMethods {
 
     }
 
-    public static void mergeSort(){
+    public static void mergeSort(int[] arr){
 
         /* 
          *        [2][4][7][1][6][3][8][5]
@@ -55,12 +56,64 @@ public class SortingMethods {
          *     [2][4]  [7][1]  [6][3]  [8][5]
          *   [2]  [4]  [7]  [1]  [6]  [3]  [8]  [5]
          *      
-         */
+        */
 
+        int length = arr.length;
+        if (length <=1 ) return; // base case
+        
+        int middle = length/2;
+        int[] letfArr = new int[middle];
+        int[] rightArr = new int[length - middle];
 
+        int i = 0; //left array
+        int j = 0; //right array
 
+        for(; i < length; i++){
+            if(i < middle){
+                letfArr[i] = arr[i];
+            }else{
+                rightArr[j] = arr[i];
+                j++;
+            }
+        }
+
+        aa++;
+        mergeSort(letfArr);
+        mergeSort(rightArr);
+        merge(arr, letfArr, rightArr);
+         
     }
 
+    private static void merge(int[] arr, int[] leftArr, int[] rightArr){
+
+        int leftSize = arr.length / 2;
+        int rightSize = arr.length - leftSize;
+        int i = 0, l = 0, r = 0; // indices
+
+        // check the conditions for merging
+
+        while(l < leftSize && r < rightSize){
+            if(leftArr[l] < rightArr[r]){
+                arr[i] = leftArr[l];
+                i++;
+                l++;
+            }else{
+                arr[i] = rightArr[r];
+                i++;
+                r++;
+            }
+        }
+        while(l < leftSize){
+            arr[i] = leftArr[l];
+            i++;
+            l++;
+        }
+        while(r < rightSize){
+            arr[i] = rightArr[r];
+            i++;
+            r++;
+        }
+    }
     
     public static void printArr(int[] arr){
 
@@ -75,15 +128,20 @@ public class SortingMethods {
 
     public static void main(String[] args) {
         
-        int[] arr = {2, 4, 7 ,1, 6, 3, 8, 5};
         // String[] arr2 = {"eae", "cebola", "ca pim", "macarrao"};
 
-        int[] arr2 = {2, 3, 4, 6, 1, 9, 8, 4, 6};
+        Integer[] arr = {2, 3, 4, 6, 1, 9, 8, 4, 6};
+        int[] arr2 = {2, 3, 5, 6, 1, 9, 8, 4, 7};
+
+        System.out.println(aa);
+        printArr(arr2);
+        mergeSort(arr2);
+        printArr(arr2);
+        System.out.println(aa);
 
 
-
-        printArr(arr);
-        intSelectionSort(arr);
+        // printArr(arr);
+        // selectionSort(arr);
         
 
     }
